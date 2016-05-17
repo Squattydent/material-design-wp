faa5b1594efb7caa4fcb97e780a3f3abf5e82eaa
Vladimir Lorentz <vladimir.lorentz@gmail.com>
2016-05-17 14:40:41 +0200

    My First Commit

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * materializecss-theme Theme Customizer.
+ *
+ * @package materializecss-theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function materializecss_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'materializecss_theme_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function materializecss_theme_customize_preview_js() {
+	wp_enqueue_script( 'materializecss_theme_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'materializecss_theme_customize_preview_js' );
