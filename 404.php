67f3730eeb1ed76df95d8a9f3be0776964524910
Vladimir Lorentz <vladimir.lorentz@gmail.com>
2016-05-17 20:18:41 +0200

    404 page and other stuff

--- 404.php
+++ 404.php
@@ -9,56 +9,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'materializecss-theme' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'materializecss-theme' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( materializecss_theme_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'materializecss-theme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'materializecss-theme' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m8 l9">
+				<div class="card">
+					<div class="card-content">
+						<h1>Code 404</h1>
+						<h3><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'materializecss-theme' ); ?></h3>
+						<p>
+							<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'materializecss-theme' ); ?>
+						</p>
+						<?php get_search_form(); ?>
+					</div>
+				</div>
+			</div>
+		<div class="col s12 m4 l3">
+		<?php get_sidebar(); ?>
+		</div>
+	</div>
+</div>
+<?php 
 get_footer();
